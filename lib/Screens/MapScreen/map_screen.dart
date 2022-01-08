a8824d31517d847de21d2134564eccd7c9ea62d9
Behruz Pulatov <behruzpulatov2014@gmail.com>
2022-01-09 00:30:10 +0500

    Updated map

--- lib/Screens/MapScreen/map_screen.dart
+++ lib/Screens/MapScreen/map_screen.dart
@@ -5,11 +5,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:easark/Screens/ProfileScreen/profile_screen.dart';
 import 'package:easark/Widgets/loading_screen.dart';
+import 'package:easark/Widgets/point_object.dart';
 import 'package:easark/Widgets/slide_right_route_animation.dart';
 import 'package:easark/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart' as geolocator;
-import 'package:easark/Widgets/ciw.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -25,15 +25,15 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  StreamSubscription? _mapIdleSubscription;
-  InfoWidgetRoute? _infoWidgetRoute;
+  double pinPillPosition = -100;
   bool loading = false;
   final Set<Marker> _markers = HashSet<Marker>();
   GoogleMapController? _mapController;
   // ignore: avoid_init_to_null
-  static LatLng _initialPosition = const LatLng(41, 60);
-  final CustomInfoWindowController _customInfoWindowController =
-      CustomInfoWindowController();
+  static LatLng _initialPosition =
+      const LatLng(41.36426966573496, 69.21005744487047);
+  BitmapDescriptor? pinLocationIcon;
+  String currentPinInfo = 'Loading ...';
 
   @override
   void initState() {
@@ -46,7 +46,6 @@ class _MapScreenState extends State<MapScreen> {
   @override
   void dispose() {
     _mapController?.dispose();
-    _customInfoWindowController.dispose();
     super.dispose();
   }
 
@@ -84,92 +83,54 @@ class _MapScreenState extends State<MapScreen> {
     }
   }
 
+  void _setMapStyle() async {
+    String style = await DefaultAssetBundle.of(context)
+        .loadString('assets/images/map_style.json');
+    _mapController?.setMapStyle(style);
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
     setState(() {
       loading = false;
     });
+    _setMapStyle();
   }
 
   void prepare() async {
     QuerySnapshot data =
         await FirebaseFirestore.instance.collection('parking_places').get();
     final places = data.docs;
+    pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+      const ImageConfiguration(devicePixelRatio: 3),
+      'assets/icons/marker.png',
+    );
 
     setState(() {
       for (QueryDocumentSnapshot place in places) {
         _markers.add(
           Marker(
-              markerId: MarkerId(place.id),
-              position: LatLng(place.get('lat'), place.get('lon')),
-              onTap: () async {
-                await _mapController?.animateCamera(
-                  CameraUpdate.newCameraPosition(
-                    CameraPosition(
-                      target: LatLng(
-                        place.get('lat') - 0.0001,
-                        place.get('lon'),
-                      ),
-                      zoom: 15,
+            markerId: MarkerId(place.id),
+            position: LatLng(place.get('lat'), place.get('lon')),
+            onTap: () async {
+              await _mapController?.animateCamera(
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: LatLng(
+                      place.get('lat') - 0.0001,
+                      place.get('lon'),
                     ),
+                    zoom: 15,
                   ),
-                );
-                // _customInfoWindowController.addInfoWindow!(
-                //   // ignore: avoid_unnecessary_containers
-                //   Container(
-                //     height: 30,
-                //     child: Card(
-                //       shape: RoundedRectangleBorder(
-                //         borderRadius: BorderRadius.circular(20.0),
-                //       ),
-                //       elevation: 10,
-                //       child: Padding(
-                //         padding: const EdgeInsets.all(20),
-                //         child: Row(
-                //           mainAxisAlignment: MainAxisAlignment.center,
-                //           children: [
-                //             Text(
-                //               place.get('ppm').toString() +
-                //                   place.get('currency').toString(),
-                //               maxLines: 2,
-                //               overflow: TextOverflow.ellipsis,
-                //               style: GoogleFonts.montserrat(
-                //                 textStyle: const TextStyle(
-                //                   color: darkColor,
-                //                   fontSize: 28,
-                //                   fontWeight: FontWeight.w600,
-                //                 ),
-                //               ),
-                //             ),
-                //             const SizedBox(
-                //               width: 10,
-                //             ),
-                //             CupertinoButton(
-                //                 child: const Icon(
-                //                   CupertinoIcons.info_circle,
-                //                   color: darkColor,
-                //                 ),
-                //                 onPressed: () {
-                //                   Navigator.push(
-                //                     context,
-                //                     SlideRightRoute(
-                //                       page: ProfileScreen(),
-                //                     ),
-                //                   );
-                //                 }),
-                //           ],
-                //         ),
-                //       ),
-                //     ),
-                //   ),
-                //   LatLng(
-                //     place.get('lat') - 0.0001,
-                //     place.get('lon'),
-                //   ),
-                // );
-              },
-              infoWindow: InfoWindow(
-                  title: place.get('ppm').toString() + place.get('currency'))),
+                ),
+              );
+              setState(() {
+                currentPinInfo = place.get('ppm').toString() + ' ' + place.get('currency');
+                pinPillPosition = 100;
+              });
+            },
+            icon: pinLocationIcon!,
+          ),
         );
       }
     });
@@ -185,7 +146,7 @@ class _MapScreenState extends State<MapScreen> {
               children: <Widget>[
                 GoogleMap(
                   mapType: MapType.normal,
-                  minMaxZoomPreference: const MinMaxZoomPreference(10.0, 40.0),
+                  minMaxZoomPreference: const MinMaxZoomPreference(5.0, 40.0),
                   myLocationEnabled: true,
                   myLocationButtonEnabled: true,
                   mapToolbarEnabled: false,
@@ -195,30 +156,69 @@ class _MapScreenState extends State<MapScreen> {
                     zoom: 15,
                   ),
                   markers: _markers,
-                  onCameraMove: (newPosition) {
-                    _mapIdleSubscription?.cancel();
-                    _mapIdleSubscription =
-                        Future.delayed(const Duration(milliseconds: 150))
-                            .asStream()
-                            .listen((_) {
-                      if (_infoWidgetRoute != null) {
-                        Navigator.of(context, rootNavigator: true)
-                            .push(_infoWidgetRoute!)
-                            .then<void>(
-                          (newValue) {
-                            _infoWidgetRoute = null;
-                          },
-                        );
-                      }
+                  onTap: (LatLng location) {
+                    setState(() {
+                      currentPinInfo = 'Loading ...';
+                      pinPillPosition = -100;
                     });
                   },
                 ),
-                // CustomInfoWindow(
-                //   controller: _customInfoWindowController,
-                //   height: 75,
-                //   width: 150,
-                //   offset: 50,
-                // ),
+                AnimatedPositioned(
+                  bottom: pinPillPosition,
+                  right: 0,
+                  left: 0,
+                  duration: const Duration(milliseconds: 200),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40.0),
+                        ),
+                        elevation: 10,
+                        child: Padding(
+                          padding: const EdgeInsets.all(0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                currentPinInfo,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.montserrat(
+                                  textStyle: const TextStyle(
+                                    color: darkColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              CupertinoButton(
+                                child: const Icon(
+                                  CupertinoIcons.arrow_right_circle_fill,
+                                  color: darkColor,
+                                  size: 40,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    SlideRightRoute(
+                                      page: ProfileScreen(),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
